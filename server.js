16720856ae2ce867b43813d60b1bdd177fd79445
shubh1777 <114337784+shubh1777@users.noreply.github.com>
2024-02-24 13:27:31 +0530

    connect express app to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express=require('express');
-const dotenv=require('dotenv');
+const dotenv=require('dotenv').config();
 const errorHandler=require('./Middleware/errorHandler');
+const connectDB = require('./config/dbConnection');
 
 const port= process.env.PORT || 3000;
 
+connectDB();
 const app=express();
 app.use(express.json());
 app.use('/api/contacts',require("./routes/contactRoutes"))
